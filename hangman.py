7d385a897e4e7fb006624de86de29ccb41b347da
Cabrini Verren <cabriniverren@gmail.com>
2026-04-22 22:21:52 +0200

    docker file, ci cd trry1

--- hangman.py
+++ hangman.py
@@ -1,12 +1,10 @@
 import random
-from collections import Counter
 
 def choose_word(word):
-    #Casefold to ignore upper and lowercase!
+    # Casefold to ignore upper and lowercase!
     return random.choice(word).casefold()
 
 def update_guessed(guessed, guess):
-    # input all the letter appear in the word. e.g if apple has double p then guessed will have pp 
     guessed.add(guess)
     return guessed
     
